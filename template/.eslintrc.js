21aeb512b77aa0ef812e16367b78411b70614b2d
zouyang <zouyang123>
2018-03-13 16:37:30 +0800

    提交代码

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -69,6 +69,9 @@ module.exports = {
     }],
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+          'indent': ["error", 2],
+          'semi': ["error","always"],
+          'space-before-function-paren': ["error",{"anonymous": "always", "named": "never"}]
   }
 }
